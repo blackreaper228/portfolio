7048539962d320b2c51ae40d0aec8be11b47dd95
blackreaper228 <ntsvetnikov@gmail.com>
2024-10-30 03:10:20 +0300

    load_test

--- docs/index.js
+++ docs/index.js
@@ -44,124 +44,137 @@ const image_namespaceObject = __webpack_require__.p + "images/6373cac8eccfb40124
 ;// CONCATENATED MODULE: ./src/index.js
 
 
-document.getElementById('nav_kolya').addEventListener('click', function () {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-});
-document.getElementById('nav_adc').addEventListener('click', function () {
-  var ADC = document.getElementById('ADC');
-  var elementPosition = ADC.getBoundingClientRect().top + window.scrollY;
-  var offset = window.innerWidth * 0.02773; // vw
-
-  window.scrollTo({
-    top: elementPosition - offset,
-    behavior: 'smooth'
-  });
-});
-document.getElementById('nav_pog').addEventListener('click', function () {
-  var pog = document.getElementById('pog');
-  var elementPosition = pog.getBoundingClientRect().top + window.scrollY;
-  var offset = window.innerWidth * 0.02773; // vw
-
-  window.scrollTo({
-    top: elementPosition - offset,
-    behavior: 'smooth'
-  });
-});
-document.getElementById('nav_chat').addEventListener('click', function () {
-  var chat = document.getElementById('chat');
-  var elementPosition = chat.getBoundingClientRect().top + window.scrollY;
-  var offset = window.innerWidth * 0.02773; // vw
-
-  window.scrollTo({
-    top: elementPosition - offset,
-    behavior: 'smooth'
-  });
-});
-document.getElementById('nav_jinx').addEventListener('click', function () {
-  var jinx = document.getElementById('jinx');
-  var elementPosition = jinx.getBoundingClientRect().top + window.scrollY;
-  var offset = window.innerWidth * 0.02773; // vw
-
-  window.scrollTo({
-    top: elementPosition - offset,
-    behavior: 'smooth'
-  });
-});
-document.getElementById('nav_nice').addEventListener('click', function () {
-  var nice = document.getElementById('nice');
-  var elementPosition = nice.getBoundingClientRect().top + window.scrollY;
-  var offset = window.innerWidth * 0.02773; // vw
-
-  window.scrollTo({
-    top: elementPosition - offset,
-    behavior: 'smooth'
-  });
-});
-window.addEventListener('scroll', function () {
-  var adc = document.getElementById('ADC');
-  var pog = document.getElementById('pog');
-  var chat = document.getElementById('chat');
-  var jinx = document.getElementById('jinx');
-  var nice = document.getElementById('nice');
-  var nav_adc = document.getElementById('nav_adc');
-  var nav_pog = document.getElementById('nav_pog');
-  var nav_chat = document.getElementById('nav_chat');
-  var nav_jinx = document.getElementById('nav_jinx');
-  var nav_nice = document.getElementById('nav_nice'); // Получаем позиции элемента ADC относительно окна просмотра
-
-  var adcRect = adc.getBoundingClientRect();
-  var pogRect = pog.getBoundingClientRect();
-  var chatRect = chat.getBoundingClientRect();
-  var jinxRect = jinx.getBoundingClientRect();
-  var niceRect = nice.getBoundingClientRect(); // Проверяем, виден ли элемент ADC в пределах видимости
-
-  if (adcRect.top < window.innerHeight && adcRect.bottom > 0) {
-    // Меняем цвет текста на белый
-    nav_adc.classList.add('highlight');
-  } else {
-    // Восстанавливаем цвет текста на черный, если элемент не виден
-    nav_adc.classList.remove('highlight');
-  }
-
-  if (pogRect.top < window.innerHeight && pogRect.bottom > 0) {
-    // Меняем цвет текста на белый
-    nav_pog.classList.add('highlight');
-  } else {
-    // Восстанавливаем цвет текста на черный, если элемент не виден
-    nav_pog.classList.remove('highlight');
-  }
-
-  if (chatRect.top < window.innerHeight && chatRect.bottom > 0) {
-    // Меняем цвет текста на белый
-    nav_chat.classList.add('highlight');
-  } else {
-    // Восстанавливаем цвет текста на черный, если элемент не виден
-    nav_chat.classList.remove('highlight');
-  }
-
-  if (jinxRect.top < window.innerHeight && jinxRect.bottom > 0) {
-    // Меняем цвет текста на белый
-    nav_jinx.classList.add('highlight');
-  } else {
-    // Восстанавливаем цвет текста на черный, если элемент не виден
-    nav_jinx.classList.remove('highlight');
-  }
-
-  if (niceRect.top < window.innerHeight && niceRect.bottom > 0) {
-    // Меняем цвет текста на белый
-    nav_nice.classList.add('highlight');
-  } else {
-    // Восстанавливаем цвет текста на черный, если элемент не виден
-    nav_nice.classList.remove('highlight');
-  }
-});
 document.addEventListener('DOMContentLoaded', function () {
-  var image = new Image();
-  image.src = image_namespaceObject;
-  document.querySelector('.images').appendChild(image);
+  document.getElementById('nav_kolya').addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  document.getElementById('nav_adc').addEventListener('click', function () {
+    var ADC = document.getElementById('ADC');
+    var elementPosition = ADC.getBoundingClientRect().top + window.scrollY;
+    var offset = window.innerWidth * 0.02773; // vw
+
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
+  });
+  document.getElementById('nav_pog').addEventListener('click', function () {
+    var pog = document.getElementById('pog');
+    var elementPosition = pog.getBoundingClientRect().top + window.scrollY;
+    var offset = window.innerWidth * 0.02773; // vw
+
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
+  });
+  document.getElementById('nav_chat').addEventListener('click', function () {
+    var chat = document.getElementById('chat');
+    var elementPosition = chat.getBoundingClientRect().top + window.scrollY;
+    var offset = window.innerWidth * 0.02773; // vw
+
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
+  });
+  document.getElementById('nav_jinx').addEventListener('click', function () {
+    var jinx = document.getElementById('jinx');
+    var elementPosition = jinx.getBoundingClientRect().top + window.scrollY;
+    var offset = window.innerWidth * 0.02773; // vw
+
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
+  });
+  document.getElementById('nav_nice').addEventListener('click', function () {
+    var nice = document.getElementById('nice');
+    var elementPosition = nice.getBoundingClientRect().top + window.scrollY;
+    var offset = window.innerWidth * 0.02773; // vw
+
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
+  });
+  window.addEventListener('scroll', function () {
+    var adc = document.getElementById('ADC');
+    var pog = document.getElementById('pog');
+    var chat = document.getElementById('chat');
+    var jinx = document.getElementById('jinx');
+    var nice = document.getElementById('nice');
+    var nav_adc = document.getElementById('nav_adc');
+    var nav_pog = document.getElementById('nav_pog');
+    var nav_chat = document.getElementById('nav_chat');
+    var nav_jinx = document.getElementById('nav_jinx');
+    var nav_nice = document.getElementById('nav_nice'); // Получаем позиции элемента ADC относительно окна просмотра
+
+    var adcRect = adc.getBoundingClientRect();
+    var pogRect = pog.getBoundingClientRect();
+    var chatRect = chat.getBoundingClientRect();
+    var jinxRect = jinx.getBoundingClientRect();
+    var niceRect = nice.getBoundingClientRect(); // Проверяем, виден ли элемент ADC в пределах видимости
+
+    if (adcRect.top < window.innerHeight && adcRect.bottom > 0) {
+      // Меняем цвет текста на белый
+      nav_adc.classList.add('highlight');
+    } else {
+      // Восстанавливаем цвет текста на черный, если элемент не виден
+      nav_adc.classList.remove('highlight');
+    }
+
+    if (pogRect.top < window.innerHeight && pogRect.bottom > 0) {
+      // Меняем цвет текста на белый
+      nav_pog.classList.add('highlight');
+    } else {
+      // Восстанавливаем цвет текста на черный, если элемент не виден
+      nav_pog.classList.remove('highlight');
+    }
+
+    if (chatRect.top < window.innerHeight && chatRect.bottom > 0) {
+      // Меняем цвет текста на белый
+      nav_chat.classList.add('highlight');
+    } else {
+      // Восстанавливаем цвет текста на черный, если элемент не виден
+      nav_chat.classList.remove('highlight');
+    }
+
+    if (jinxRect.top < window.innerHeight && jinxRect.bottom > 0) {
+      // Меняем цвет текста на белый
+      nav_jinx.classList.add('highlight');
+    } else {
+      // Восстанавливаем цвет текста на черный, если элемент не виден
+      nav_jinx.classList.remove('highlight');
+    }
+
+    if (niceRect.top < window.innerHeight && niceRect.bottom > 0) {
+      // Меняем цвет текста на белый
+      nav_nice.classList.add('highlight');
+    } else {
+      // Восстанавливаем цвет текста на черный, если элемент не виден
+      nav_nice.classList.remove('highlight');
+    }
+  }); // const image = new Image()
+  // image.src = jpg
+  // document.querySelector('.images').appendChild(image)
+
+  document.querySelectorAll('.Q_BigPic').forEach(function (img) {
+    var placeholder = img.src; // Изображение, которое загружается по умолчанию
+    // Обработчик загрузки основной версии
+
+    img.addEventListener('load', function () {
+      img.classList.add('loaded'); // Когда загружено, делаем изображение видимым
+    }); // Заменяем на основную версию после загрузки миниатюры
+
+    var fullSrc = img.getAttribute('src');
+    img.setAttribute('src', placeholder); // Показываем миниатюру
+
+    img.src = fullSrc; // Подменяем на основное изображение
+  });
 });
 /******/ })()
 ;
